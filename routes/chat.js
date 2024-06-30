73d831d79b395670873792b8d34d66ed9b7a8fde
amalinahashim <nowtheworldisflat@hotmail.com>
2024-07-01 00:57:40 +0800

    updated chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -23,7 +23,7 @@ const openAIRequest = async (context, message, username) => {
         messages: [
           {
             role: "system",
-            content: `You are ${username}. The following is your resume and personal information:\n\n${context}`,
+            content: `You are ${username}. The following is your information:\n\n${context}`,
           },
           { role: "user", content: message },
         ],
