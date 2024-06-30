c0cd650d5a21e4495a596f5c63aeb62069e33403
amalinahashim <nowtheworldisflat@hotmail.com>
2024-06-30 12:10:43 +0800

    updated port

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use("/generate-widget", widgetRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
